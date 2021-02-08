9014c1e8804eee3953648212712c4c48b372768a
Sean McLemon <sean.mclemon@gmail.com>
2021-02-08 19:28:51 +0100

    initial (and probably ONLY) commit

---
+++ Interview.LetterSequencer.CSharp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+    From : https://danielbmarkham.com/fun-with-an-interview-question/
+
+        Most candidates cannot solve this interview problem:
+
+            Input: "aaaabbbcca"
+            Output: [("a", 4), ("b", 3), ("c", 2), ("a", 1)]
+        
+            Write a function that converts the input to the output I ask it in the screening interview and give it 25 minutes How would you solve it?
+        
+    I deliberately kept this REALLY simple - the way I'd do it if I was in an interview situation and knew I needed an easy
+    to debug program. Nothing fancy, does the job ... but very verbose. I suspect the interviewer would have some additional
+    questions and try to nudge me to rework it.
+ */
+
+static IEnumerable<(char, int)> ConvertToSequence(string input)
+{
+    var result = new List<(char, int)>();
+
+    char? currentChar = null;
+    int count = -1;
+
+    foreach (var c in input)
+    {
+        if (!currentChar.HasValue)
+        {
+            currentChar = c;
+            count = 1;
+        }
+        else if (currentChar == c)
+        {
+            count++;
+        }
+        else
+        {
+            result.Add(
+                (currentChar.Value, count)
+            );
+            currentChar = c;
+            count = 1;
+        }
+    }
+
+    result.Add(
+        (currentChar.Value, count)
+        );
+
+    return result;
+}
+
+static string SerializeSequence(IEnumerable<(char, int)> sequence)
+{
+    var res = new List<string>();
+
+    foreach (var (c, count) in sequence)
+    {
+        res.Add($"(\"{c}\", {count})");
+    }
+
+    return $"[{string.Join(", ", res)}]";
+}
+
+var sequence = ConvertToSequence("aaaabbbcca");
+var answer = SerializeSequence(sequence);
+
+Console.WriteLine("[(\"a\", 4), (\"b\", 3), (\"c\", 2), (\"a\", 1)]");
+Console.WriteLine(answer);
---
+++ Interview.LetterSequencer.FSharp/Program.fs
@@ -0,0 +1,38 @@
+﻿(*
+From : https://danielbmarkham.com/fun-with-an-interview-question/
+
+    Most candidates cannot solve this interview problem:
+
+        Input: "aaaabbbcca"
+        Output: [("a", 4), ("b", 3), ("c", 2), ("a", 1)]
+    
+        Write a function that converts the input to the output I ask it in the screening interview and give it 25 minutes How would you solve it?
+    
+After doing the C# version I realised it's probably a bit harder in F#. Again the following is quick-n-dirty and I put it together while on a call
+but I think it's quite representative of what I'd do in an interview - nothing fancy, just using the basics well enough to produce a relatively
+legible bit of code.
+*)
+
+let accumulate (current:char, count:int, seen:(char * int) list) next = 
+    if current = next
+    then (current, 1 + count, seen)
+    else (next, 1, seen @ [(current, count)])
+
+let occurrences' (str:string) = 
+    str.ToCharArray()
+    |> Array.toList
+    |> List.fold accumulate ('!', 0, []) // lol
+
+let occurrences str =
+    let curr, cnt, seen = occurrences' str
+    seen @ [curr, cnt]
+    |> List.skip 1    
+
+[<EntryPoint>]
+let main argv =
+    printfn "[(\"a\", 4), (\"b\", 3), (\"c\", 2), (\"a\", 1)]"
+    
+    occurrences "aaaabbbcca"
+    |> printfn "%A" // separator + quotes are different, but w/e
+
+    0 // return an integer exit code
